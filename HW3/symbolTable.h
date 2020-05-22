272165ea801089ce2325a99b52141e4871a96db0
Tania <drob.tania@gmail.com>
2020-05-22 18:09:45 +0300

    symbolTable basic implementation

--- HW3/symbolTable.h
+++ HW3/symbolTable.h
@@ -4,31 +4,69 @@
 
 #ifndef HW3_SYMBOLTABLE_H
 #define HW3_SYMBOLTABLE_H
+#include "parser.hpp"
 
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
-
-class TableEntry
-{};
-
 
 
 class SymbolTable {
 public:
-    vector <Scope> stack;
+    vector <Scope*> stack;
 
-    void newScope(){}
+    SymbolTable() {}
+
+    void newScope(){
+
+        Scope* scopeToAdd;
+        if (stack.empty()){
+            scopeToAdd = new Scope(0);
+        }
+        else{
+            scopeToAdd = new Scope(stack.back()->offset);
+        }
+        stack.push_back(scopeToAdd);
+    }
     void endScope(){}
+
+    void addSymbolVar( Node symbolToAdd ){
+        stack.back()->addSymbolVar( symbolToAdd );
+    }
+    void addSymbolFunc( Node symbolToAdd ){
+        stack.back()->addSymbolFunc( symbolToAdd );
+    }
 };
 
 class Scope{
-
+public:
     map<string, TableEntry> entries;
-    Scope(){}
-}
+    int offset;
 
+    Scope(int offset) : offset(offset) {}
+
+    void addSymbolVar( Node symbolToAdd ){
+        TableEntry* entryToAdd = new TableEntry( symbolToAdd );
+        entries.emplace ( symbolToAdd.name, entryToAdd );
+    }
+    void addSymbolFunc( Node funcToAdd ){
+        TableEntry* entryToAdd = new TableEntry( funcToAdd );
+        entries.emplace ( funcToAdd.name, entryToAdd );
+    }
+};
+
+class TableEntry{
+public:
+
+    string name;
+    TableEntry( Node newNode ){
+        name = newNode.name;
+        cout<<"new table entry added: "<<name<<endl;
+    }
+
+};
 
 #endif //HW3_SYMBOLTABLE_H
