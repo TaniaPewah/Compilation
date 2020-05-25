29cf98d09e4a314ab3f3921f782510e0fdd72e3d
Tania <drob.tania@gmail.com>
2020-05-25 22:19:53 +0300

    parser adapter running the symboltable

--- HW3/parserAdapter.hpp
+++ HW3/parserAdapter.hpp
@@ -7,6 +7,13 @@
 
 SymbolTable symbolTable;
 
+IdNode* ruleFuncDecl(IdNode* id_node) {
+    string name = id_node->name;
+	IdNode* current_node = new IdNode(id_node->lineno, name); 
+	symbolTable.addSymbolFunc( current_node );
+    delete(id_node);
+    return current_node;
+}
 
 IdNode* ruleVarDecl(IdNode* id_node) {
     string name = id_node->name;
@@ -15,5 +22,15 @@ IdNode* ruleVarDecl(IdNode* id_node) {
     delete(id_node);
     return current_node;
 }
+
+IdNode* ruleVarDeclAssign(IdNode* id_node) {
+    string name = id_node->name;
+	IdNode* current_node = new IdNode(id_node->lineno, name); 
+	symbolTable.addSymbolVar( current_node );
+    delete(id_node);
+    return current_node;
+}
+
+
 
 #endif //PARSER_ADAPTER_HPP
